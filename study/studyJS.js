ebd365cd11fec428d93b050d9efd3262c5e60d37
Ryota Kumashiro <ryota.kumashiro@gmail.com>
2022-08-19 09:45:55 +0900

    JS復習用の課題を追加

---
+++ study/studyJS.js
@@ -0,0 +1,71 @@
+window.onload = function () {
+  //課題１
+  //拡張For文を使って「apple」だけをコンソールに出力してください。
+  let fluits = ["banana", "lemmon", "apple", "grape", "banana"]
+  console.log("-----------------------kadai1-----------------------")
+  //ここに課題１のソースコード記載↓
+
+
+  //課題２
+  //拡張For文を使って「banana」を２回コンソールに出力してください。
+  console.log("-----------------------kadai2-----------------------")
+  //ここに課題２のソースコード記載↓
+
+
+  //課題３
+  //拡張For文を使って「idが3のユーザの氏名」をコンソールに出力してください。
+  var user_json_list = [
+    {
+      id: 1,
+      last_name: "田中",
+      first_name: "太郎",
+      gender: 1,
+      birth_date: "1999-01-01",
+      address: "東京都",
+    },
+    {
+      id: 2,
+      last_name: "佐藤",
+      first_name: "花子",
+      gender: 2,
+      birth_date: "1995-01-01",
+      address: "埼玉県",
+    },
+    {
+      id: 3,
+      last_name: "伊藤",
+      first_name: "二郎",
+      gender: 1,
+      birth_date: "1997-01-01",
+      address: "北海道",
+    },
+    {
+      id: 4,
+      last_name: "山田",
+      first_name: "優子",
+      gender: 3,
+      birth_date: "1989-01-01",
+      address: "千葉県",
+    },
+    {
+      id: 5,
+      last_name: "山田",
+      first_name: "一郎",
+      gender: 1,
+      birth_date: "1999-01-01",
+      address: "東京都",
+    },
+  ]
+  console.log("-----------------------kadai3-----------------------")
+  //ここに課題３のソースコード記載↓
+
+
+  //課題４
+  //拡張For文とoutputFirstNameを使って男性のユーザのファーストネームだけをコンソールに出力してください。
+  function outputFirstName(user_json) {
+    console.log(user_json.first_name)
+  }
+  console.log("-----------------------kadai4-----------------------")
+  //ここに課題４のソースコード記載↓
+
+}

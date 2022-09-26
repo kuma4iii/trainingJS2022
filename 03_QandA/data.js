fe205d6d386acc431f0f205e815a17e1918b911d
Ryota Kumashiro <ryota.kumashiro@gmail.com>
2022-09-26 11:50:42 +0900

    ファイル整理、新規課題用データ格納

---
+++ 03_QandA/data.js
@@ -0,0 +1,30 @@
+var user_list = [
+    {
+        id: 1,
+        name: "田中（JavaScript、angular）",
+        user_id: "user01",
+        password: "password",
+        image_name: "boy_02.png"
+    },
+    {
+        id: 2,
+        name: "yamamoto yoko",
+        user_id: "user02",
+        password: "password",
+        image_name: "girl_18.png"
+    },
+    {
+        id: 3,
+        name: "Koukiフリーランス",
+        user_id: "user03",
+        password: "password",
+        image_name: "youngman_33.png"
+    },
+    {
+        id: 4,
+        name: "ヤマモト",
+        user_id: "user01",
+        password: "password",
+        image_name: "youngman_35.png"
+    },
+]
